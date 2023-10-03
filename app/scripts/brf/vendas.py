b003e3e1ca283d02360f370cee2fc06122669b83
Marcelo Galli <99439779+MarceloGalliDev@users.noreply.github.com>
2023-10-03 09:54:35 -0300

    update
    update

--- app/scripts/brf/vendas.py
+++ app/scripts/brf/vendas.py
@@ -72,9 +72,9 @@ class Vendas:
             AND clie.clie_cepres NOT IN ('00000-000','','0','00000','00000000')
             AND clie.clie_cepres > '0'
             AND clie.clie_cepres NOT IN ('')
-            AND mprd.mprd_datamvto > CURRENT_DATE - INTERVAL '10 DAYS'
         """)
-            # AND mprd.mprd_datamvto > '2023-01-09'
+            # AND mprd.mprd_datamvto > '2021-01-01'
+            # AND mprd.mprd_datamvto > CURRENT_DATE - INTERVAL '10 DAYS'
         
         df = pd.read_sql_query(query, self.conn)
         return df
@@ -165,8 +165,8 @@ class Vendas:
     def vendas(self):
         get_tables_query = self.generate_sql_query()
         for unid_codigo in self.unid_codigos:
-            tables = get_tables_query
-            # tables = ['movprodd0122', 'movprodd0222', 'movprodd0322', 'movprodd0422', 'movprodd0522', 'movprodd0622', 'movprodd0722', 'movprodd0822', 'movprodd0922', 'movprodd1022', 'movprodd1122', 'movprodd1222', 'movprodd0123', 'movprodd0223', 'movprodd0323', 'movprodd0423', 'movprodd0523', 'movprodd0623', 'movprodd0723', 'movprodd0823', 'movprodd0923', 'movprodd1023',  'movprodd1123', 'movprodd1223']
+            # tables = get_tables_query
+            tables = ['movprodd0121', 'movprodd0221', 'movprodd0321', 'movprodd0421', 'movprodd0521', 'movprodd0621', 'movprodd0721', 'movprodd0821', 'movprodd0921', 'movprodd1021', 'movprodd1121', 'movprodd1221','movprodd0122', 'movprodd0222', 'movprodd0322', 'movprodd0422', 'movprodd0522', 'movprodd0622', 'movprodd0722', 'movprodd0822', 'movprodd0922', 'movprodd1022', 'movprodd1122', 'movprodd1222', 'movprodd0123', 'movprodd0223', 'movprodd0323', 'movprodd0423', 'movprodd0523', 'movprodd0623', 'movprodd0723', 'movprodd0823', 'movprodd0923', 'movprodd1023',  'movprodd1123', 'movprodd1223']
             
             dfs = [self.vendas_query(table, self.conn, unid_codigo) for table in tables]
             dfs = [df.dropna(axis=1, how='all') for df in dfs]
