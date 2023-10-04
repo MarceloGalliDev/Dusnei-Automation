30505c7aaf63b0cec37a32fee7659b07aa438d77
Marcelo Galli <99439779+MarceloGalliDev@users.noreply.github.com>
2023-10-04 15:49:00 -0300

    update
    update

--- app/scripts/brf/produtos.py
+++ app/scripts/brf/produtos.py
@@ -44,6 +44,7 @@ class Produtos:
                 WHERE prun.prun_bloqueado = 'N' 
                 AND prun.prun_unid_codigo IN ({unid_values})
                 AND prod.prod_marca IN ('BRF','BRF IN NATURA')
+                AND prod.prod_status = 'N'
         """)
         return pd.read_sql_query(query, conn)
 
@@ -70,7 +71,7 @@ class Produtos:
                 cod_barras_tipo = "1"
             
             nome_produto = (row["produto"] or "")[:100].ljust(100)
-            razao_forn2 = ('BRF S.A.')[:40].ljust(40)
+            razao_forn2 = ("BRF S.A.")[:40].ljust(40)
             estoque_qtde = row["estoque1"]
             status_prod = "A" if estoque_qtde > 0 else "I"
 
